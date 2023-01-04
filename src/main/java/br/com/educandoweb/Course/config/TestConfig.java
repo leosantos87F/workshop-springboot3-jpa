a2df6b31de5e845d2e735d7da5e3ffccd14fdf6d
Leonardo Santos <leosantos.vinte@gmail.com>
2023-01-04 16:54:07 -0300

    Payment, one-to-one association

--- src/main/java/br/com/educandoweb/Course/config/TestConfig.java
+++ src/main/java/br/com/educandoweb/Course/config/TestConfig.java
@@ -11,6 +11,7 @@ import org.springframework.context.annotation.Profile;
 import br.com.educandoweb.Course.entities.Category;
 import br.com.educandoweb.Course.entities.Order;
 import br.com.educandoweb.Course.entities.OrderItem;
+import br.com.educandoweb.Course.entities.Payment;
 import br.com.educandoweb.Course.entities.Product;
 import br.com.educandoweb.Course.entities.User;
 import br.com.educandoweb.Course.entities.enums.OrderStatus;
@@ -82,6 +83,10 @@ public class TestConfig implements CommandLineRunner {
 		 
 		orderItemRepository.saveAll(Arrays.asList(oi1, oi2, oi3, oi4));
 		
+		Payment pay1 = new Payment(null, Instant.parse("2019-06-20T21:53:07Z"), o1 );
+		o1.setPayment(pay1);
+		
+		orderRepository.save(o1);
 		
 		
 
